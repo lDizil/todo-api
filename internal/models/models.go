eac2e8fb9736a069fa8f7123b2738dd40fc0d0f1
Dizi <plastinin.denis2009@mail.ru>
2025-09-04 11:20:52 +0300

    feat: Доделаны все методы для работы с todo структурой, дополнительно создана структуру для обновления todo

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,7 +1,17 @@
 package models
 
+import "time"
+
 type Todo struct {
-	ID          string `json:"id" db:"id"`
-	TaskName    string `json:"taskName" db: "taskName"`
-	Description string `json:"Description" db:"Description"`
+	ID          string    `json:"id" db:"id"`
+	TaskName    string    `json:"taskName" db:"taskName"`
+	Description *string   `json:"description" db:"description"`
+	Completed   bool      `json:"completed" db:"completed"`
+	CreatedAt   time.Time `json:"createdAt" db:"createdAt"`
+}
+
+type UpdateTodoRequest struct {
+	TaskName    *string `json:"name,omitempty"`
+	Description *string `json:"description,omitempty"`
+	Completed   *bool   `json:"completed,omitempty"`
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,24 +1,105 @@
 package repository
 
 import (
-	"todo-api/internal/models"
 	"errors"
+	"todo-api/internal/models"
 )
 
 type TodoRepository interface {
 	Create(task *models.Todo) error
 	GetById(id string) (*models.Todo, error)
-	Update(task *models.Todo) error
+	Update(id string, updateData *models.UpdateTodoRequest) error
 	Delete(id string) error
 	GetAllTask() ([]*models.Todo, error)
 }
 
 type StorageRepository struct {
-	todos map[string] *models.Todo
+	todos map[string]*models.Todo
 }
 
 func Constructor() *StorageRepository {
-	return $StorageRepository{
-		todos: make(map[string] *models.Todo),
+	return &StorageRepository{
+		todos: make(map[string]*models.Todo),
 	}
+}
+
+var errEmptyID = errors.New("передан пустой айди")
+
+func (s *StorageRepository) Create(task *models.Todo) error {
+	if task == nil {
+		return errors.New("передана пустая задача")
+	}
+
+	if task.ID == "" {
+		return errEmptyID
+	}
+
+	if _, exists := s.todos[task.ID]; exists {
+		return errors.New("задача с таким айди уже существует")
+	}
+
+	s.todos[task.ID] = task
+
+	return nil
+}
+
+func (s *StorageRepository) GetById(id string) (*models.Todo, error) {
+	if id == "" {
+		return nil, errEmptyID
+	}
+
+	if task, exists := s.todos[id]; exists {
+		return task, nil
+	}
+
+	return nil, errors.New("задача с таким айди не найдена")
+}
+
+func (s *StorageRepository) Update(id string, updateData *models.UpdateTodoRequest) error {
+	if id == "" {
+		return errEmptyID
+	}
+
+	if updateData == nil {
+		return errors.New("необходимо передать данные для обновления")
+	}
+
+	if task, exists := s.todos[id]; exists {
+		if updateData.Completed != nil {
+			task.Completed = *updateData.Completed
+		}
+		if updateData.Description != nil {
+			task.Description = updateData.Description
+		}
+		if updateData.TaskName != nil {
+			task.TaskName = *updateData.TaskName
+		}
+	} else {
+		return errors.New("задача с таким айди не найдена")
+	}
+
+	return nil
+}
+
+func (s *StorageRepository) Delete(id string) error {
+	if id == "" {
+		return errEmptyID
+	}
+
+	if _, exists := s.todos[id]; exists {
+		delete(s.todos, id)
+		return nil
+	}
+
+	return errors.New("задача для удаления с таким айди не найдена")
+}
+
+func (s *StorageRepository) GetAllTask() ([]*models.Todo, error) {
+	var result []*models.Todo
+
+	for _, task := range s.todos {
+		result = append(result, task)
+	}
+
+	return result, nil
 }
