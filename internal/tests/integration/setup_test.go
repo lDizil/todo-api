a135448cffce99b4ed9a07fdd63a08afbddcded2
Dizi <plastinin.denis2009@mail.ru>
2025-10-06 10:46:48 +0300

    test: написание интеграционных тестов; реализован setup для настройки среды; реализовано несколько тестов

--- internal/tests/integration/setup_test.go
+++ internal/tests/integration/setup_test.go
@@ -1,35 +1,72 @@
 package integration_tests
 
 import (
+	"database/sql"
 	"log"
+	"os"
 	"testing"
 	"todo-api/internal/config"
+	"todo-api/internal/models"
+	"todo-api/migrations"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	_ "github.com/lib/pq"
 )
 
-var testDB *gorm.DB
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	cfg := config.Load()
 
-	testDB, err := gorm.Open(postgres.Open(cfg.Database.TestDSN()), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent)
-	})
-
+	var err error
+	testDB, err = sql.Open("postgres", cfg.Database.TestDSN())
 	if err != nil {
 		log.Fatalf("Failed to connect to test database: %v", err)
 	}
 
-	if err := testDB.AutoMigrate(&models.Todo{}); err != nil {
-        log.Fatalf("Failed to migrate test database: %v", err)
-    }
+	if err := testDB.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	if err := migrations.RunMigrations(testDB); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
 
 	code := m.Run()
-	
-	cleanupDatabase()
+
+	CleanUpDatabase()
+	testDB.Close()
 
 	os.Exit(code)
+}
+
+func SetUpTest(t *testing.T) *sql.DB {
+	t.Helper()
+
+	_, err := testDB.Exec("TRUNCATE TABLE todos RESTART IDENTITY CASCADE")
+	if err != nil {
+		t.Fatalf("Failed to truncate table: %v", err)
+	}
+
+	return testDB
+}
+
+func CleanUpDatabase() {
+	testDB.Exec("DROP TABLE IF EXISTS todos CASCADE")
+}
+
+func CreateTestTodo(db *sql.DB, taskName string, description *string) *models.Todo {
+	todo := &models.Todo{
+		TaskName:    taskName,
+		Description: description,
+		Completed:   false,
+	}
+
+	query := "INSERT INTO todos (task_name, description, completed) VALUES ($1, $2, $3) RETURNING id, created_at"
+	err := db.QueryRow(query, todo.TaskName, todo.Description, todo.Completed).Scan(&todo.ID, &todo.CreatedAt)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return todo
 }
---
+++ internal/tests/integration/todo_integration_test.go
@@ -0,0 +1,144 @@
+package integration_tests
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+	"todo-api/internal/handlers"
+	"todo-api/internal/models"
+	"todo-api/internal/repository"
+	"todo-api/internal/services"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func setUpRouter(db *sql.DB) *gin.Engine {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.New()
+
+	todoRepo := repository.NewPostgresRepository(db)
+	todoService := services.NewTodoService(todoRepo)
+	todoHandler := handlers.NewTodoHandler(todoService)
+
+	router.POST("/todos", todoHandler.CreateTodo)
+	router.GET("/todos/:id", todoHandler.GetById)
+	router.GET("/todos", todoHandler.GetAllTask)
+	router.PATCH("/todos/:id", todoHandler.Update)
+	router.DELETE("/todos/:id", todoHandler.Delete)
+
+	return router
+}
+
+func TestCreateTodo_Integration(t *testing.T) {
+	db := SetUpTest(t)
+	router := setUpRouter(db)
+
+	reqBody := `{"taskName":"integration test","description":"test description"}`
+	req := httptest.NewRequest("POST", "/todos", bytes.NewBufferString(reqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+
+	var response models.Todo
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, response.ID)
+	assert.NotEmpty(t, response.CreatedAt)
+	assert.False(t, response.Completed)
+	assert.Equal(t, "integration test", response.TaskName)
+	assert.Equal(t, "test description", response.Description)
+
+	var todoInDB models.Todo
+	qeury := "SELECT id, task_name, description, completed, created_at FROM todos WHERE id=$1"
+	err = db.QueryRow(qeury, response.ID).Scan(
+		&todoInDB.ID,
+		&todoInDB.TaskName,
+		&todoInDB.Description,
+		&todoInDB.Completed,
+		&todoInDB.CreatedAt,
+	)
+
+	assert.NoError(t, err)
+	assert.Equal(t, response.ID, todoInDB.ID)
+	assert.Equal(t, "integration test", todoInDB.TaskName)
+	assert.Equal(t, "test description", *todoInDB.Description)
+}
+
+func TestGetByID_Integration(t *testing.T) {
+	db := SetUpTest(t)
+	router := setUpRouter(db)
+
+	description := "test description"
+	todo := CreateTestTodo(db, "test task", &description)
+
+	req := httptest.NewRequest("GET", "/todos/"+todo.ID, nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	var response *models.Todo
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+	assert.Equal(t, "test task", response.TaskName)
+	assert.Equal(t, "test description", response.Description)
+}
+
+func TestGetAllTask_Integration(t *testing.T) {
+	db := SetUpTest(t)
+	router := setUpRouter(db)
+
+	description1 := "test description1"
+	_ = CreateTestTodo(db, "test task1", &description1)
+
+	description2 := "test description2"
+	_ = CreateTestTodo(db, "test task2", &description2)
+
+	req := httptest.NewRequest("GET", "/todos", nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	var response []*models.Todo
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NotEmpty(t, response[0].ID)
+	assert.NotEmpty(t, response[0].TaskName)
+	assert.NoError(t, err)
+	assert.Equal(t, "test task1", response[0].TaskName)
+	assert.Equal(t, "test description1", response[0].Description)
+}
+
+func TestUpdateTodo_Integration(t *testing.T) {
+	db := SetUpTest(t)
+	router := setUpRouter(db)
+
+	description := "test description"
+	todo := CreateTestTodo(db, "test task", &description)
+
+	reqBody := `{"taskName":"integration test new","description":"test description new", "completed":"true"}`
+	req := httptest.NewRequest("PATCH", "/todos/" + todo.ID,  bytes.NewBufferString(reqBody))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	var response *models.Todo
+
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+	assert.Equal(t, "integration test new", response.TaskName)
+	assert.Equal(t, "test description new", response.Description)
+	assert.NotEmpty(t, response.ID)
+	assert.NotEmpty(t, response.TaskName)
+}
+
+func TestDeleteTodo_
